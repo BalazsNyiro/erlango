26c9c3c61a8d989de5bc279a31c1c00a4c12d853
Balazs Nyiro <diogenesz@pergamen.hu>
2023-03-11 22:35:57 -0500

    test

--- erlango/erlango_test.go
+++ erlango/erlango_test.go
@@ -8,7 +8,10 @@ LICENSE file in the root directory of this source tree.
 */
 package erlango
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Test_ParseErlangSourceFile(t *testing.T) {
 	received := ParseErlangSourceFile()
@@ -19,4 +22,30 @@ func compare_int_pair(received, wanted int, t *testing.T) {
 	if received != wanted {
 		t.Fatalf("\nreceived: %v\n  wanted: %v, error", received, wanted)
 	}
+}
+
+///////// go experimental tests
+
+func Test_struct_modifications(_ *testing.T) {
+
+	// this is a trial: what happens with structs after modifications?
+	var chars []ErlSrcChar
+	A := ErlSrcChar{Value: 'a'}
+
+	chars = append(chars, A)
+
+	// question 1: is chars[0] object same with A?
+	fmt.Printf("Address of struct       A = %+v: %p\n", A, &A)
+	fmt.Printf("Address of struct chars[0]= %+v: %p\n", chars[0], &chars[0])
+	/*  at this point the address of the two objects are different:
+	    Address of struct       A = {NextChar:<nil> PrevChar:<nil> PosInFile:0 Value:97 Token:<nil>}: 0xc000014510
+	    Address of struct chars[0]= {NextChar:<nil> PrevChar:<nil> PosInFile:0 Value:97 Token:<nil>}: 0xc000014540
+	*/
+
+	A.PosInFile = 1
+	fmt.Printf("after position in file change in A:\n")
+	fmt.Printf("Address of struct       A = %+v: %p\n", A, &A)
+	fmt.Printf("Address of struct chars[0]= %+v: %p\n", chars[0], &chars[0])
+
+	// result: when I append an elem into a slice, a copy is inserted.
 }
