f37c74a0a763f59108d0b1828b1cbaa3de520f38
Balazs Nyiro <balazs.nyiro.ca@gmail.com>
2023-04-02 22:02:38 -0400

    whitespace tests ON

--- erlango/test/parse/erlang_whitespaces_separators.erl
+++ erlango/test/parse/erlang_whitespaces_separators.erl
@@ -3,16 +3,16 @@
 %INFO tested:
 %INFO newline | tab | space | comma | dot | semicolon
        % Token_type_whitespace
-          %INFO Token_type_whitespace (newline as last char)
+          % Token_type_whitespace (newline as last char)
 main(_) ->
-%INFO Token_type_whitespace
+% Token_type_whitespace
 	io:format(example(), "tab indentation instead of space, before io:format").
 
                        %INFO Token_type_comma
-                        %INFO Token_type_whitespace (space)
+                        % Token_type_whitespace (space)
     io:format(example(), "comma token in the line").
-          %INFO Token_type_whitespace (space)
-             %INFO Token_type_whitespace (space)
+          % Token_type_whitespace (space)
+             % Token_type_whitespace (space)
                       %INFO Token_type_semicolon
 example(1) -> "case 1";
                            %INFO Token_type_dot
