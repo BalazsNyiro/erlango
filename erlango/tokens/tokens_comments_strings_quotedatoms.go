fc1dd474452b47abaf81b211048a674338edef78
Balazs Nyiro <balazs.nyiro.ca@gmail.com>
2024-01-07 14:55:20 -0500

    token detection, strings

--- erlango/tokens/tokens_comments_strings_quotedatoms.go
+++ erlango/tokens/tokens_comments_strings_quotedatoms.go
@@ -13,12 +13,19 @@ Version 0.3, third total rewrite
 
 package tokens
 
+import "fmt"
+
 type Token struct {
 	tokenName          string
+
+	// "quoted" string's first " is the tokens' first position!
+	// 'atoms'  fist ' char is the tokens first pos!
+	// so ALL character is included from the src into the token position range
 	positionCharFirst  int
 	positionCharLast   int
-	sourceFile         string
-	textRepresentation string
+
+	sourceFile    string
+	charsInErlSrc []rune
 }
 
 /*Tokens represent the Erlang source code - so the int-key is the first char's position in the source code*/
@@ -27,6 +34,10 @@ type Tokens map[int]Token
 /*
 	Token detection rules - in every step, a group of tokens are removed only, so all tokens
 							are removed in MORE steps.
+
+	At the beginning, the input is the original Erlang source code, and tokensTable is empty
+	in every step, Erlang source code has more and more empty sections, as charsInErlSrc are detected,
+    and the token table has more and more elems with detected tokens
 
 	input:   Actual, maybe cleaned Erlang source code, previously detected tokens.
 	process: from the source code, the characters of detected tokens are relocated into Tokens
@@ -40,5 +51,37 @@ type Tokens map[int]Token
 Receives Erlang source code - return with non-detected source code and detected Tokens.
 */
 func Tokens_detect_comments_strings_quotedatoms(erlSrc string, tokensTable Tokens) (string, Tokens){
+	// commentLineCloser := "\n"
+
+	inActiveDetection := ""
+
+	tokenNow := Token{}
+
+	for charPos, charRune := range erlSrc {
+		fmt.Println("pos:", charPos, charRune)
+		if charRune == '"' {
+
+			if inActiveDetection == "" {  // if there is NO active detection
+				inActiveDetection = "stringDoubleQuoted"
+				tokenNow = Token{positionCharFirst: charPos}
+				continue
+			}
+
+			if inActiveDetection == "stringDoubleQuoted" {
+				inActiveDetection = ""  // close the active string detection
+				tokenNow.positionCharLast = charPos
+				tokenPosStart := tokenNow.positionCharFirst
+				tokensTable[tokenPosStart] = tokenNow
+			}
+		}
+
+
+		/////////////////////////////////////////////////////////////////////
+		if inActiveDetection != "" { // so if there is an active detection
+				tokenNow.charsInErlSrc = append(tokenNow.charsInErlSrc, charRune)
+
+		}
+
+	}
 	return erlSrc, tokensTable
 }
--- erlango/tokens/tokens_comments_strings_quotedatoms_test.go
+++ erlango/tokens/tokens_comments_strings_quotedatoms_test.go
@@ -12,3 +12,55 @@ Version 0.3, third total rewrite
 */
 
 package tokens
+
+import (
+	"fmt"
+	"testing"
+)
+
+
+func Test_parse_comments_textDoubleQuoted_textSingleQuoted(t *testing.T) {
+
+	erlSrc :=`	
+		VarAtomQuoted = 'atomValue1', 
+		VarStr  = "string"	
+		[A, B, StringValue] = [1, 2, "This is a line with a string"]
+
+	`
+	tokensTable := Tokens{}
+
+	erlSrc_commentsStringsAtomsquoted_removed, tokensTableXXX := Tokens_detect_comments_strings_quotedatoms(erlSrc, tokensTable)
+
+	fmt.Println("erlSrc, without strings, quoted atoms", erlSrc_commentsStringsAtomsquoted_removed)
+
+	fmt.Println("tokensTableOriginal:", tokensTable)
+	fmt.Println("tokensTableUpdated:", tokensTableXXX)
+
+}
+
+/*
+func compare_tokenDetected_tokenWanted(callerInfo string, tokensDetected ErlTokens, tokenWanted tokenWanted, t *testing.T) {
+	tokenDetected, tokenWantedIsInDetected:= tokensDetected[tokenWanted.positionFirst]
+
+	if tokenWantedIsInDetected {
+		// theoretically the charPosFirst is always ok here, because the key in map was the same position
+		tokenDetected_charPosFirst, tokenDetected_charPosLast := tokenDetected.charPositionFirstLast()
+		if tokenDetected_charPosFirst != tokenWanted.positionFirst {
+			t.Fatalf("\nErr First: %s : detected posFirst: %v  is different from wanted posFirst:  %v, error:\n'%s'\n'%s'\n\n",
+				callerInfo, tokenDetected_charPosFirst, tokenWanted.positionFirst, tokenDetected.stringRepresentation(), tokenWanted.textRepresentation)
+		}
+		if tokenDetected_charPosLast != tokenWanted.positionLast {
+			t.Fatalf("\nErr Last: %s : detected posLast: %v  is different from wanted posLast:  %v, error:\n'%s'\n'%s'\n\n",
+				callerInfo, tokenDetected_charPosLast, tokenWanted.positionLast, tokenDetected.stringRepresentation(), tokenWanted.textRepresentation)
+		}
+		if tokenDetected.stringRepresentation() != tokenWanted.textRepresentation {
+			t.Fatalf("\nErr repr %s : startPos:%v  detected string representation: %v  is different from wanted representation:  %v, error",
+				callerInfo, tokenDetected_charPosFirst, tokenDetected.stringRepresentation(), tokenWanted.textRepresentation)
+		}
+	} else {
+		t.Fatalf("\nErr %s : wanted tokenPosFirst %v is not in detecteds - error", callerInfo, tokenWanted.positionFirst)
+	}
+
+}
+
+ */
