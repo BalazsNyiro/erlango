75f16e3e07331ae32b9c9147d03a9ffe3b74bffb
Balazs Nyiro <balazs.nyiro.ca@gmail.com>
2024-01-21 12:32:24 -0500

    number_tests

--- erlango/tokens/tokens_detect_numbers_test.go
+++ erlango/tokens/tokens_detect_numbers_test.go
@@ -30,7 +30,7 @@ func Test_parse_numbers_int_simple(t *testing.T) {
 	print_tokens("tokens table " + testName, tokensTable_02_intDetected)
 
 
-	fmt.Println("erlSrc, without strings, quoted atoms", erlSrc_received_after_tokenDetect)
+	fmt.Println("after simple int detection, erlSrc: ", erlSrc_received_after_tokenDetect)
 
 	fmt.Println("tokensTableOriginal:", tokensTable)
 	fmt.Println("tokensTableUpdated:", tokensTable_02_intDetected)
@@ -54,7 +54,7 @@ func Test_parse_numbers_int_with_underscore(t *testing.T) {
 	print_tokens("tokens table: " + testName, tokensTable_02_intDetected)
 
 
-	fmt.Println("erlSrc, without strings, quoted atoms", erlSrc_received_after_tokenDetect)
+	fmt.Println("after int detection, erlSrc", erlSrc_received_after_tokenDetect)
 
 	fmt.Println("tokensTableOriginal:", tokensTable)
 	fmt.Println("tokensTableUpdated:", tokensTable_02_intDetected)
@@ -63,6 +63,33 @@ func Test_parse_numbers_int_with_underscore(t *testing.T) {
 	compare_strings(testName, "1_1", tokensTable_02_intDetected[10].stringRepr(), t)
 	compare_strings(testName, "2_2_2", tokensTable_02_intDetected[15].stringRepr(), t)
 	compare_strings(testName, "34_5", tokensTable_02_intDetected[22].stringRepr(), t)
+
+	fmt.Println(testName, erlSrcWantedAfterTokenDetect, erlSrcOrig, tokensTable)
+}
+
+
+func Test_parse_numbers_floats(t *testing.T) {
+	testName := "parse_numbers 03, float detection"
+
+	tokensTable := Tokens{}
+	erlSrcOrig :=                    `A, B, C, D = 11.2_2, 33.44, 5_5.66, 7_7.8_8.`
+	erlSrcWantedAfterTokenDetect :=  `A, B, C, D =       ,      ,       ,        .`
+
+	erlSrc_received_after_tokenDetect, tokensTable_02_detected := Tokens_detect_numbers(erlSrcOrig, tokensTable)
+	print_string_runes_diff(erlSrcOrig, erlSrc_received_after_tokenDetect)
+	print_tokens("tokens table: " + testName, tokensTable_02_detected)
+
+
+	fmt.Println("after float detection, erlSrc:", erlSrc_received_after_tokenDetect)
+
+	fmt.Println("tokensTableOriginal:", tokensTable)
+	fmt.Println("tokensTableUpdated:", tokensTable_02_detected)
+
+	compare_strings(testName, erlSrcWantedAfterTokenDetect, erlSrc_received_after_tokenDetect, t)
+	compare_strings(testName, "11.2_2", tokensTable_02_detected[13].stringRepr(), t)
+	compare_strings(testName, "33.44", tokensTable_02_detected[21].stringRepr(), t)
+	compare_strings(testName, "5_5.66", tokensTable_02_detected[28].stringRepr(), t)
+	compare_strings(testName, "7_7.8_8", tokensTable_02_detected[36].stringRepr(), t)
 
 	fmt.Println(testName, erlSrcWantedAfterTokenDetect, erlSrcOrig, tokensTable)
 }
